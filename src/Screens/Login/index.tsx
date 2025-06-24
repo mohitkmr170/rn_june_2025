558f403d6bc00056f5125901436ca273f63f4dad
Mohit Kumar <mohitkmr170@gmail.com>
2025-06-24 14:55:08 +0530

    feat : react-hook-form added, ui added for login screen

--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -1,15 +1,81 @@
-import {Text, TouchableOpacity, View} from 'react-native';
+import {Button, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import {styles} from './styles';
 import {useNavigation} from '@react-navigation/native';
+import {Controller, useForm} from 'react-hook-form';
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 const LoginScreen = () => {
   const navigation = useNavigation();
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<FormData>();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
 
   return (
     <View style={styles.parentContainer}>
-      <TouchableOpacity onPress={() => (navigation as any).navigate('Home')}>
-        <Text>Login Screen</Text>
-      </TouchableOpacity>
+      <Controller
+        control={control}
+        name="email"
+        rules={{
+          required: 'Email is required!',
+          pattern: {
+            value: /^\S+@\S+$/i,
+            message: 'Invalid email address',
+          },
+        }}
+        render={({field: {onChange, value}}) => {
+          return (
+            <TextInput
+              style={styles.inputContainer}
+              onChangeText={onChange}
+              value={value}
+              keyboardType="email-address"
+              autoCapitalize={'none'}
+              placeholder="Email"
+            />
+          );
+        }}
+      />
+      {errors.email?.message && (
+        <Text style={styles.error}>{errors.email.message}</Text>
+      )}
+      <Controller
+        control={control}
+        name="password"
+        rules={{
+          required: 'Password is required!',
+          minLength: {
+            value: 6,
+            message: 'Password must be at least 6 characters',
+          },
+        }}
+        render={({field: {onChange, value}}) => {
+          return (
+            <TextInput
+              style={styles.inputContainer}
+              onChangeText={onChange}
+              value={value}
+              keyboardType="email-address"
+              autoCapitalize={'none'}
+              placeholder="Password"
+              secureTextEntry
+            />
+          );
+        }}
+      />
+      {errors.password?.message && (
+        <Text style={styles.error}>{errors.password.message}</Text>
+      )}
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
   );
 };
--- src/Screens/Login/styles.ts
+++ src/Screens/Login/styles.ts
@@ -3,7 +3,16 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   parentContainer: {
     flex: 1,
+    margin: 16,
     justifyContent: 'center',
-    alignItems: 'center',
   },
+  inputContainer: {
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 12,
+    borderRadius: 8,
+    fontSize: 18,
+    lineHeight: 22,
+  },
+  error: {color: 'red', marginBottom: 10},
 });
